19adff39786b09ebbfbc10fd9e900a7d9b5b0609
Michael Salvati <print@3daddict.com>
2018-09-12 18:03:48 -0700

    completed

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,13 @@ import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
 
+import Greeting from './greeting'
+
 const App = () => (
     <div>
         <div className="app">
+            <Greeting name="Jim Bob" favColor="firebrick" />
+            <Greeting name={1542} favColor="orange" />
             <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
         </div>
