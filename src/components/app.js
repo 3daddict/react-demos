974cdca226860e0633cce54df035bb3482066974
Michael Salvati <print@3daddict.com>
2018-09-14 11:55:53 -0700

    working clock on dom

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../assets/css/app.css';
+import logo from '../assets/images/logo.svg'
 import Clock from './clock';
 
 const App = () => (
-    <div>
-        <Clock />
-    </div>
+    <div className="app">
+            <img src={logo} className="logo rotate"/>
+            <Clock/>
+        </div>
 );
 
 export default App;
