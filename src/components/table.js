28e083856f3c7604db2c8e05421da3f6f42e86f9
Michael Salvati <print@3daddict.com>
2018-09-13 21:17:38 -0700

    working on creating the table.js

---
+++ src/components/table.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Table(){
+    return (
+    <table className="table">
+        <thead className="thead-inverse">
+            <th>Name</th>
+            <th>Course</th>
+            <th>Grade</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Jill</td>
+                <td>Math</td> 
+                <td>50</td>
+            </tr>
+            <tr>
+                <td>Mat</td>
+                <td>English</td> 
+                <td>98</td>
+            </tr>
+            <tr>
+                <td>Geraldo</td>
+                <td>Geography</td> 
+                <td>80</td>
+            </tr>
+        </tbody>
+    </table>
+    )
+}
+
+export default Table();
