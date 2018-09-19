0674970bdddb45aba91db5b153098dac28499457
Michael Salvati <print@3daddict.com>
2018-09-19 13:36:40 -0700

    styles images challenege completed

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import '../assets/css/app.css';
+import '../assets/css/app_styles.css';
 import logo from '../assets/images/logo.svg';
+import skate from '../assets/images/skate.jpg';
+
+const greenStyle = {
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    fontSize: '58px',
+    color: 'green'
+}
 
 const App = () => (
     <div>
         <div className="app">
             <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
+            <div className="comp1">
+            <p className="c1">Functional Component 1</p>
+            </div>
+            <div className="comp2">
+            <p className="c2">Functional Component 2</p>
+            </div>
+            <div>
+            <p style={greenStyle}>Functional Component 3</p>
+            <img src={skate} alt="skater"/>
+            </div>
         </div>
     </div>
 );
